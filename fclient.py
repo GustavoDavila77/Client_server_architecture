2eb65aff1c8957fd15fd8c6669c2d49aa9ae4bcc
Gustavo Dávila <davila7@utp.edu.co>
2020-08-28 10:39:28 -0500

    exist verified

--- fclient.py
+++ fclient.py
@@ -35,10 +35,13 @@ elif cmd == 'download':
     filetodownload = sys.argv[2]
     socket.send_multipart([bytes("download", encoding='utf-8'), bytes(filetodownload, encoding='utf-8')])
     resp = socket.recv_multipart()
-    filename = resp[1].decode('utf-8')
-    print(filename)
-    with open('downloads/'+filename, 'wb') as f:
-            f.write(resp[2])
-    print("archivo descargado")
+    if len(resp) == 3:
+        filename = resp[1].decode('utf-8')
+        print(filename)
+        with open('downloads/'+filename, 'wb') as f:
+                f.write(resp[2])
+        print("archivo descargado")
+    else:
+        print(resp)
 else:
     print('Error, comando no valido')
--- fserver.py
+++ fserver.py
@@ -8,7 +8,7 @@ socket.bind("tcp://*:5555") #se enlaza por medio del protocolo tcp y va a respon
 
 print("Socket created!!!")
 
-files = []
+#files = []
 archivos = []
 # 1. upload a file
 # 2. list files
@@ -16,19 +16,21 @@ archivos = []
 
 while True:
     message = socket.recv_multipart()
-    print(message)
+    directorio = os.listdir('D:\Escritorio\Arquitectura cliente servidor\code/files')
     if message[0] == b'upload':
         # client wants to upload a file
         filename = message[1].decode('utf-8')
-        print(filename)
-        files.append('filename')
-        with open('files/'+filename, 'wb') as f:
-            f.write(message[2])
-        socket.send_string("Listo!!")
+        if filename in directorio:
+            print("exist")
+            socket.send_string("File already exist, change name")
+        else:
+            print("don´t exist")
+            with open('files/'+filename, 'wb') as f:
+                f.write(message[2])
+                socket.send_string("Ready!!")
+
+        
     elif message[0] == b'list':
-        directorio = os.listdir('D:\Escritorio\Arquitectura cliente servidor\code/files')
-        print(directorio)
-        #socket.send_multipart([b"listfiles", directorio.encode('utf-8'), bytes])
         for f in directorio:
             archivos.append(f.encode('utf-8'))
 
@@ -36,9 +38,13 @@ while True:
     elif message[0] == b'download':
         filename = message[1].decode('utf-8')
         print(filename)
-        with open('files/'+ filename,'rb') as f:
-            bytes = f.read()
-            socket.send_multipart([b"downloadfile", filename.encode('utf-8'), bytes])      
+        try:
+            with open('files/'+ filename,'rb') as f:
+                bytes = f.read()
+                socket.send_multipart([b"downloadfile", filename.encode('utf-8'), bytes])
+        except:
+            socket.send_multipart([b"Notfound"])
+
     else:
         print('Error!!')
         socket.send_string("Error")
